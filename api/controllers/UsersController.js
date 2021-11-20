6eacfe3df82a2aaa60e475ac75f2094c38a3bccb
Nilsharr <48214683+Nilsharr@users.noreply.github.com>
2021-11-21 00:02:45 +0100

    Fix fix reset code validation

--- api/controllers/UsersController.js
+++ api/controllers/UsersController.js
@@ -112,8 +112,8 @@ exports.forgotPassword = async (req, res) => {
 }
 
 exports.resetPassword = async (req, res) => {
+    const email = req.body.email;
     const code = req.params.code;
-    const email = req.params.email;
     if (!code || !email) {
         return res.status(400).send({ error: "Invalid data" });
     }
