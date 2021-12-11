d6f67026fec323e02d11a6cde4381d9f607bdeaf
unknown <maciekrekiel@gmail.com>
2021-12-11 12:29:16 +0100

    CORS added

--- Server.js
+++ Server.js
@@ -2,18 +2,18 @@ require("./database/DatabaseService");
 const userRoutes = require("./api/routes/UsersRoutes");
 const eventRoutes = require("./api/routes/EventsRoutes");
 const express = require("express");
+const cors = require("cors");
 const port = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors());
 
 userRoutes(app);
 eventRoutes(app);
 
 module.exports = app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
-
-
